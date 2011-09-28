7d15ffb02590e0a95f03939088e4469a427f2fea
codingisacopingstrategy <eric@ericschrijver.nl>
2011-09-28 18:44:55 +0200

    3-lingual (:

--- flyer+site/language.js
+++ flyer+site/language.js
@@ -1,6 +1,30 @@
-function language() {
+function defaultLanguage() {
     var possible = ["nl", "en", "fr"];
     var index = possible.indexOf(navigator.language);
     var language = index !== -1 ? possible[index] : "en"
     return language
 }
+
+function showLanguage(language) {
+    $("div#text > div").each( function() {
+        if ($(this).attr("class") !== language) {
+            $(this).hide();
+            $("div#select_language ul li." + $(this).attr("class")).removeClass("active");
+        }
+        else {
+            if ($(this).is(':hidden')) {
+                $(this).show();
+            }
+            $("div#select_language ul li." + language).addClass("active");
+        }
+    });
+};
+
+$(function() {
+    showLanguage(defaultLanguage());
+
+    $("div#select_language ul li").click(function(event){
+        showLanguage($(this).attr("class"));
+    })
+})
+
